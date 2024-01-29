68e94680b4d00ae7e6103d4471816a7b76b62ff2
uday-maddeni <muday03122001@gmail.com>
2024-01-29 08:43:00 +0530

    Product component (Cards component) is done

--- 4.Angular/Demo/src/app/showempbyid/showempbyid.component.ts
+++ 4.Angular/Demo/src/app/showempbyid/showempbyid.component.ts
@@ -18,7 +18,8 @@ export class ShowempbyidComponent implements OnInit {
       {empId: 102, empName:'Pasha',  salary:2323.23, gender:'Male',   doj:'2017-10-16', country:'China',    emailId:'pasha@gmail.com',  password:'123'},
       {empId: 103, empName:'Indira', salary:3434.34, gender:'Female', doj:'2016-09-17', country:'USA',      emailId:'indira@gmail.com', password:'123'},
       {empId: 104, empName:'Vamsi',  salary:4545.45, gender:'Male',   doj:'2015-08-18', country:'SriLanka', emailId:'vamsi@gmail.com',  password:'123'},
-      {empId: 105, empName:'Venkat', salary:5656.56, gender:'Male',   doj:'2014-07-19', country:'Nepal',    emailId:'venkat@gmail.com', password:'123'}
+      {empId: 105, empName:'Venkat', salary:5656.56, gender:'Male',   doj:'2014-07-19', country:'Nepal',    emailId:'venkat@gmail.com', password:'123'},
+      {empId: 106, empName:'Rahul', salary:5656.56, gender:'Male',   doj:'2014-07-19', country:'Nepal',    emailId:'rahul@gmail.com', password:'123'}
     ];
   }
 
