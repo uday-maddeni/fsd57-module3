57602f630adabd3deab09fe17e15d9d3518c03e8
uday-maddeni <muday03122001@gmail.com>
2024-02-09 23:41:42 +0530

    Demo Angular project is done

--- 4.Angular/Demo/src/app/image.service.ts
+++ 4.Angular/Demo/src/app/image.service.ts
@@ -1,39 +1,3 @@
-// // image.service.ts
-
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class ImageService {
-//   private baseUrl = 'http://localhost:8085/image';
-
-//   constructor(private httpClient: HttpClient) {}
-
-//   uploadImage(imageFile: File, category: string): Observable<any> {
-//     const uploadImageData = new FormData();
-//     uploadImageData.append('imageFile', imageFile, imageFile.name);
-//     uploadImageData.append('category', category);
-
-//     return this.httpClient.post(`${this.baseUrl}/upload`, uploadImageData, { observe: 'response' });
-//   }
-
-//   getImage(imageName: string): Observable<any> {
-//     return this.httpClient.get(`${this.baseUrl}/get/${imageName}`);
-//   }
-
-//   getAllImages(): Observable<any[]> {
-//     return this.httpClient.get<any[]>(`${this.baseUrl}/getAllImages`);
-//   }
-
-//   getImagesByCategory(category: string): Observable<any[]> {
-//     return this.httpClient.get<any[]>(`${this.baseUrl}/getImagesByCategory/${category}`);
-//   }
-// }
-
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
